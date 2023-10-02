45fef12e879e726698741a2569d639a4459b22b1
Hyeokjune Jeon <hyeokjune.jeon@furiosa.ai>
2023-10-02 19:09:10 +0900

    include-dir

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 // src/main.rs
 use anyhow::{self};
 use clap::{App, Arg};
-use log::info;
+use log::{debug, info};
 use std::collections::HashMap;
 use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
 use tera::{Context, Tera};
-
 mod plugin;
 use plugin::{Plugin, PluginFunction};
 mod error;
@@ -22,6 +23,7 @@ struct Args {
     plugin: Option<String>,
     output_file: Option<String>,
     disable_builtin_functions: bool,
+    include_dirs: Vec<(String, Option<String>)>,
 }
 
 fn parse_arguments() -> Args {
@@ -77,6 +79,13 @@ fn parse_arguments() -> Args {
                 .long("disable-builtin-functions")
                 .help("Disables the registration of built-in functions"),
         )
+        .arg(
+            Arg::with_name("include-dir")
+                .long("include-dir")
+                .multiple(true)
+                .takes_value(true)
+                .help("Include directory for templates. Format: /path/to/dir:alias or /path/to/dir. Use '{}' for direct naming without an alias."),
+        )
         .get_matches();
 
     let mut envs = HashMap::new();
@@ -115,6 +124,15 @@ fn parse_arguments() -> Args {
         }
     }
 
+    let include_dirs = matches
+        .values_of("include-dir")
+        .unwrap_or_default()
+        .map(|s| {
+            let parts: Vec<&str> = s.splitn(2, ':').collect();
+            (parts[0].to_string(), parts.get(1).map(|&s| s.to_string()))
+        })
+        .collect();
+
     Args {
         envs,
         template: matches.value_of("template").unwrap().to_string(),
@@ -122,7 +140,31 @@ fn parse_arguments() -> Args {
         plugin: matches.value_of("plugin").map(|s| s.to_string()),
         output_file: matches.value_of("output_file").map(ToOwned::to_owned),
         disable_builtin_functions: matches.is_present("disable_builtin_functions"),
+        include_dirs,
     }
+}
+
+fn add_templates_from_dir(tera: &mut Tera, dir: &Path, alias: Option<&str>) -> anyhow::Result<()> {
+    let files: Vec<_> = fs::read_dir(dir)?
+        .filter_map(Result::ok)
+        .map(|entry| entry.path())
+        .filter(|path| path.is_file())
+        .collect();
+
+    for file in files {
+        let template_name = file.strip_prefix(dir).unwrap().to_str().unwrap();
+        let template_name_with_alias = match alias {
+            Some(alias_str) => format!("{}.{}", alias_str, template_name),
+            None => template_name.to_string(),
+        };
+        debug!(
+            "added tempate: {:?} => {:?}",
+            template_name, template_name_with_alias
+        );
+        tera.add_template_files(vec![(file, Some(template_name_with_alias))])?;
+    }
+
+    Ok(())
 }
 
 fn main() -> anyhow::Result<()> {
@@ -130,9 +172,31 @@ fn main() -> anyhow::Result<()> {
     panic_hook();
 
     let args = parse_arguments();
-    let mut tera = Tera::default();
     let mut context = Context::new();
+    let mut tera = Tera::default();
+    for (dir_str, alias_opt) in args.include_dirs.iter() {
+        let dir_path = Path::new(dir_str);
+        let default_alias = dir_path
+            .file_name()
+            .and_then(|name| name.to_str())
+            .unwrap_or("")
+            .to_string();
 
+        let alias = match alias_opt {
+            Some(alias_string) if alias_string == "{}" => None,
+            Some(other_alias) => Some(other_alias.clone()),
+            None => Some(default_alias),
+        };
+
+        info!("{:?} => {:?} from {:?}", dir_path, alias, alias_opt);
+
+        add_templates_from_dir(&mut tera, dir_path, alias.as_deref())?;
+    }
+
+    info!(
+        "tempate names: {:?}",
+        tera.get_template_names().collect::<Vec<&str>>()
+    );
     if !args.disable_builtin_functions {
         function::register_functions(&mut tera);
     }
